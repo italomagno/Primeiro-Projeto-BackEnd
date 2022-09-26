94167754524d566e65063434667b5b98595abff3
Italo Magno <italomagno10@gmail.com>
2022-09-26 14:03:35 -0300

    creating deposit
    foi criado o post de deposito, utilizando o middleware para verificar se existe uma conta cpf válida.

--- src/index.js
+++ src/index.js
@@ -61,6 +61,23 @@ if(costumerAlreadyExists){
 })
 
 
+app.post("/deposit", verifyIfExistsAccountCPF, (request,response)=>{
+  const { description, amount} = request.body;
+  const {customer} = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation);
+
+  response.status(201).send()
+})
+
+
 
 
 //segunda forma de usar o middleware
